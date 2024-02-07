2a6c4d8c2ad287c95419ff291a69347b6e069d57
free-diy <71208560+free-diy@users.noreply.github.com>
2024-02-07 12:06:37 +0800

    Create diy-part1.sh

---
+++ diy-part1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+#echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+#echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
+#echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> "feeds.conf.default"
+#echo "src-git helloworld https://github.com/fw876/helloworld.git;master" >> "feeds.conf.default"
