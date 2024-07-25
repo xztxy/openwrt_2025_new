c039a18205b2f7168afd4fbb7ed270531d6e28f9
free-diy <71208560+free-diy@users.noreply.github.com>
2024-07-25 08:18:26 +0800

    Update x86.sh

--- scripts/x86.sh
+++ scripts/x86.sh
@@ -63,8 +63,8 @@ git clone --depth=1 https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 # 添加SmartDNS
 git clone --depth=1 -b lede https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
 git clone --depth=1 https://github.com/pymumu/openwrt-smartdns package/smartdns
-# 添加adguardhome,bypass
-git_sparse_clone main https://github.com/kenzok8/small-package luci-app-adguardhome luci-app-bypass lua-maxminddb
+# 添加adguardhome,bypass，文件管理助手
+git_sparse_clone main https://github.com/kenzok8/small-package luci-app-adguardhome luci-app-bypass lua-maxminddb luci-app-fileassistant
 # 添加ddns-go
 git clone --depth=1 https://github.com/sirpdboy/luci-app-ddns-go package/ddns-go
 # 添加istore
