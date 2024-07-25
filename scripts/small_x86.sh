cb8ec4c92c7a22202854d2bb9603dd5e6cee264c
free-diy <71208560+free-diy@users.noreply.github.com>
2024-07-25 08:20:40 +0800

    Update small_x86.sh

--- scripts/small_x86.sh
+++ scripts/small_x86.sh
@@ -63,8 +63,10 @@ git clone --depth=1 -b main https://github.com/sirpdboy/luci-app-advancedplus pa
 # 添加SmartDNS
 #git clone --depth=1 -b lede https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
 #git clone --depth=1 https://github.com/pymumu/openwrt-smartdns package/smartdns
-# 添加adguardhome,bypass
-#git_sparse_clone main https://github.com/kenzok8/small-package luci-app-adguardhome luci-app-bypass lua-maxminddb
+# 添加adguardhome
+#git_sparse_clone main https://github.com/kenzok8/small-package luci-app-adguardhome
+# bypass，文件管理助手
+git_sparse_clone main https://github.com/kenzok8/small-package luci-app-bypass lua-maxminddb luci-app-fileassistant
 # 添加ddns-go
 #git clone --depth=1 https://github.com/sirpdboy/luci-app-ddns-go package/ddns-go
 # 添加istore
