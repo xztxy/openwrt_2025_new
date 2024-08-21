2454e4ab438d7401868829a7360451385e06c0ce
free-diy <71208560+free-diy@users.noreply.github.com>
2024-08-21 16:24:52 +0800

    Update lede_small_x86.sh

--- scripts/lede_small_x86.sh
+++ scripts/lede_small_x86.sh
@@ -13,15 +13,6 @@ rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/luci/applications/luci-app-argon-config
 # 删除英文版netdata
 #rm -rf feeds/luci/applications/luci-app-netdata
-# 删除低版本golang
-#rm -rf feeds/packages/lang/golang
-# 删除低版本mosdns
-#rm -rf feeds/packages/net/v2ray-geodata
-#rm -rf feeds/packages/utils/v2dat
-#rm -rf feeds/packages/net/mosdns
-#rm -rf feeds/luci/applications/luci-app-mosdns
-# 删除低版本smartdns
-#rm -rf feeds/packages/net/smartdns
 
 ###### Git稀疏克隆
 # 参数1是分支名, 参数2是仓库地址, 参数3是子目录，同一个仓库下载多个文件夹直接在后面跟文件名或路径，空格分开
@@ -63,12 +54,6 @@ git clone --depth=1 -b main https://github.com/sirpdboy/luci-app-advancedplus pa
 #git clone --depth=1 https://github.com/sirpdboy/luci-app-ddns-go package/ddns-go
 # 设备关机功能
 git clone --depth=1 https://github.com/sirpdboy/luci-app-poweroffdevice package/luci-app-poweroffdevice
-# 添加MosDNS
-#git clone --depth=1 -b v5 https://github.com/sbwml/luci-app-mosdns package/mosdns
-#git clone --depth=1 https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-# 添加SmartDNS
-#git clone --depth=1 -b lede https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
-#git clone --depth=1 https://github.com/pymumu/openwrt-smartdns package/smartdns
 # 添加adguardhome,bypass，管控过滤,访问限制，文件管理助手等
 #git_sparse_clone main https://github.com/kenzok8/small-package luci-app-adguardhome luci-app-bypass luci-app-fileassistant luci-app-filebrowser luci-app-timecontrol luci-app-control-timewol luci-app-control-weburl luci-app-control-webrestriction
 # 添加bypass，文件管理助手
@@ -78,10 +63,6 @@ git_sparse_clone master https://github.com/kenzok8/openwrt-packages lua-maxmindd
 # 添加istore
 git_sparse_clone main https://github.com/linkease/istore-ui app-store-ui
 git_sparse_clone main https://github.com/linkease/istore luci
-# 添加应用管理
-#git clone --depth=1 -b master https://github.com/destan19/OpenAppFilter package/OpenAppFilter
-# 添加vssr
-#git clone --depth=1 https://github.com/free-diy/luci-app-vssr package/luci-app-vssr
 
 ##### 科学上网插件
 git clone --depth=1 -b master https://github.com/fw876/helloworld package/luci-app-ssr-plus
