50a7ae27bb05a3ed45c3deb03e259f63f5e5105a
free-diy <71208560+free-diy@users.noreply.github.com>
2024-08-25 17:39:25 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,3 +12,5 @@
 #echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 #echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> "feeds.conf.default"
 #echo "src-git helloworld https://github.com/fw876/helloworld.git;master" >> "feeds.conf.default"
+#修改内核版本
+#sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=6.6/g' target/linux/x86/Makefile
