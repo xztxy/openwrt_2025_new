7a22df41de82d93a979cf18d4aa9640608e06be1
xztxy <31698398+xztxy@users.noreply.github.com>
2025-03-22 01:14:51 +0800

    Update immortalwrt_24.10_x86.sh

--- scripts/immortalwrt_24.10_x86.sh
+++ scripts/immortalwrt_24.10_x86.sh
@@ -37,6 +37,7 @@ git clone --depth=1 -b master https://github.com/jerrykuku/luci-theme-argon pack
 git clone --depth=1 -b master https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 # 拉取酷猫主题
 git clone --depth=1 -b main https://github.com/sirpdboy/luci-theme-kucat package/luci-theme-kucat
+git clone --depth=1 -b main https://github.com/sirpdboy/luci-app-kucat-config
 #拉取peditx主题
 git clone --depth=1 -b main https://github.com/peditx/luci-theme-peditx package/luci-theme-peditx
 
